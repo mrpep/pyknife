11f3ff7b9367a28f6357c0d49591656d314358c7
Leonardo <leonardodpepino@gmail.com>
2020-09-14 11:44:23 -0300

    option of entering aws credentials

--- swissknife/aws/__init__.py
+++ swissknife/aws/__init__.py
@@ -2,8 +2,14 @@ import boto3
 import tqdm
 from pathlib import Path
 
-def get_instances_info(region='us-east-1'):
-    ec2 = boto3.client('ec2',region)
+def get_instances_info(region='us-east-1',input_credentials=False):
+    if input_credentials:       
+        key_id = input('aws_access_key_id')
+        secret_key = input('aws_secret_access_key')
+        ec2 = boto3.client('ec2',region,aws_access_key_id=key_id,aws_secret_access_key=secret_key)
+    else:
+        ec2 = boto3.client('ec2',region)
+
     response = ec2.describe_instances()
     info_instances = []
     for group in response['Reservations']: 
@@ -17,8 +23,13 @@ def get_instances_info(region='us-east-1'):
 
     return info_instances 
 
-def download_s3(bucket_name, bucket_path, download_path, exclude=None, if_exists='abort'):
-    s3_client = boto3.client('s3')
+def download_s3(bucket_name, bucket_path, download_path, exclude=None, if_exists='abort',input_credentials=False):
+    if input_credentials:
+        key_id = input('aws_access_key_id')
+        secret_key = input('aws_secret_access_key')
+        s3_client = boto3.client('s3',aws_access_key_id=key_id,aws_secret_access_key=secret_key)
+    else:
+        s3_client = boto3.client('s3')
 
     all_keys = s3_client.list_objects_v2(Bucket=bucket_name,Prefix=bucket_path)
     keys = [k['Key'] for k in all_keys['Contents']]
