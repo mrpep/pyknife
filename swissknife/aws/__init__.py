ee6e20523b8cc9fe1ec3690cb22d95fff9110288
Leonardo <leonardodpepino@gmail.com>
2020-10-18 02:15:51 -0300

    get all s3 objects (not just the first 1000)

--- swissknife/aws/__init__.py
+++ swissknife/aws/__init__.py
@@ -23,6 +23,19 @@ def get_instances_info(region='us-east-1',input_credentials=False):
 
     return info_instances 
 
+def get_all_s3_objects(s3, **base_kwargs):
+    #https://stackoverflow.com/questions/54314563/how-to-get-more-than-1000-objects-from-s3-by-using-list-objects-v2/54314628
+    continuation_token = None
+    while True:
+        list_kwargs = dict(MaxKeys=1000, **base_kwargs)
+        if continuation_token:
+            list_kwargs['ContinuationToken'] = continuation_token
+        response = s3.list_objects_v2(**list_kwargs)
+        yield from response.get('Contents', [])
+        if not response.get('IsTruncated'):  # At the end of the list?
+            break
+        continuation_token = response.get('NextContinuationToken')
+
 def download_s3(bucket_name, bucket_path, download_path, exclude=None, if_exists='abort',input_credentials=False):
     if input_credentials:
         key_id = input('aws_access_key_id')
@@ -34,8 +47,9 @@ def download_s3(bucket_name, bucket_path, download_path, exclude=None, if_exists
     if exclude is None:
         exclude = []
 
-    all_keys = s3_client.list_objects_v2(Bucket=bucket_name,Prefix=bucket_path)
-    keys = [k['Key'] for k in all_keys['Contents'] if k['Key'] not in exclude and k['Key'][-1] != '/']
+    #all_keys = s3_client.list_objects_v2(Bucket=bucket_name,Prefix=bucket_path)
+    all_keys = [f for f in get_all_s3_objects(s3_client, Bucket=bucket_name, Prefix=bucket_path)]
+    keys = [k['Key'] for k in all_keys if k['Key'] not in exclude and k['Key'][-1] != '/']
 
     download_path = Path(download_path)
 
