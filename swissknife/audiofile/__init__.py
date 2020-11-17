e53a8a50dcba526c0f301c4f20cdbcd5f9d770be
mrpep <leonardodpepino@gmail.com>
2020-11-17 04:30:09 +0000

    update:

--- swissknife/audiofile/__init__.py
+++ swissknife/audiofile/__init__.py
@@ -2,7 +2,7 @@ from swissknife.console import run_command
 
 def ffmpeg_to_wav(in_file,out_file,sr=None):
     if sr is None:
-        cmd = "ffmpeg -i {} {}".format(in_file,sr,out_file)
+        cmd = "ffmpeg -y -i {} {}".format(in_file,sr,out_file)
     else:
-        cmd = "ffmpeg -i {} -ar {} {}".format(in_file,sr,out_file)
+        cmd = "ffmpeg -y -i {} -ar {} {}".format(in_file,sr,out_file)
     run_command(cmd,silent=True)
