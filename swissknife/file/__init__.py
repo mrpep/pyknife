cb7d6aa3025c4384f17fc76043dda8a7efab5949
mrpep <leonardodpepino@gmail.com>
2020-12-29 23:27:55 +0000

    fix compressed files

--- swissknife/file/__init__.py
+++ swissknife/file/__init__.py
@@ -3,10 +3,10 @@ import tqdm
 
 class CompressedFile:
     def __init__(self,path):
-        self.file = Path(path)
+        self.file = Path(path).expanduser()
         self.extension = self.file.suffix
     def extract(self,destination_path):
-        destination_path = Path(destination_path)
+        destination_path = Path(destination_path).expanduser()
         if not destination_path.exists():
             destination_path.mkdir(parents=True)
         if self.extension == '.gz':
@@ -14,14 +14,15 @@ class CompressedFile:
             tar = tarfile.open(self.file)
             tar_members = tar.getnames()
             for member in tqdm.tqdm(tar_members):
-                if not Path(destination_path,member).expanduser().exists():
+                if not Path(destination_path,member).exists():
                     tar.extract(member,destination_path)
             tar.close()
         elif self.extension == '.zip':
-			import zipfile
-			zip_file = zipfile.ZipFile(self.file)
-			zip_members = zip_file.namelist()
-			for member in tqdm.tqdm(zip_members):
-				if not Path(destination_path,member).expanduser().exists():
-					zip_file.extract(member,destination_path)
-			zip_file.close()
+            import zipfile
+            zip_file = zipfile.ZipFile(self.file)
+            zip_members = zip_file.namelist()
+            for member in tqdm.tqdm(zip_members):
+                if not Path(destination_path,member).exists():
+                    zip_file.extract(member,destination_path)
+                    print('extracting {} in {}'.format(member,destination_path))
+            zip_file.close()
